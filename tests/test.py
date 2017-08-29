f10bfb7b8bbf5e86623ff13bcd6d7a02365de3df
gahag <gahag@hotmail.com.br>
2017-08-29 18:47:48 -0300

    Added new test cases.

--- tests/test.py
+++ tests/test.py
@@ -11,9 +11,9 @@ tests_dir = os.path.dirname(os.path.realpath(__file__))
 tptester.tp_test(
   os.getcwd() + '/' + sys.argv[1],
   
-  indexes = [ '5' ],
-  input_file = lambda ix: tests_dir + '/data/in' + ix + '.txt',
-  answer_file = lambda ix: tests_dir + '/data/out' + ix + '.txt',
+  indexes = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ],
+  input_file = lambda ix: tests_dir + '/data/in/' + ix + '.in',
+  answer_file = lambda ix: tests_dir + '/data/out/' + ix + '.in',
   
   args = lambda ix: [ ],
   stdin = lambda ifile: ifile,
