413f191ebcb2f90d405f22c06f70a4f0e5bb81b1
gahag <gahag@hotmail.com.br>
2017-08-31 20:01:49 -0300

    Added new benchmark.

--- tests/test.py
+++ tests/test.py
@@ -11,9 +11,9 @@ tests_dir = os.path.dirname(os.path.realpath(__file__))
 tptester.tp_test(
   os.getcwd() + '/' + sys.argv[1],
   
-  indexes = ['10','20','30','40','50','60','70','80','90','100','110','120','130','140','150','160','170','180','190','200','210','220','230','240','250','260','270','280','290','300','310','320','330','340','350','360','370','380','390','400','410','420','430','440','450','460','470','480','490','500'],
-  input_file = lambda ix: tests_dir + '/data/bench2/' + ix + '.in',
-  answer_file = lambda ix: tests_dir + '/data/bench2/' + ix + '.out',
+  indexes = [16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144],
+  input_file = lambda ix: tests_dir + '/data/bench3/' + str(ix) + '.in',
+  answer_file = lambda ix: tests_dir + '/data/bench2/10.out',
   
   args = lambda ix: [ ],
   stdin = lambda ifile: ifile,
